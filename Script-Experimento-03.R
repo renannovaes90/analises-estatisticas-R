bb7e6ebeb267f831469d0629bf915abba821bf91
Renan Novaes <renan.cunha@protonmail.com>
2025-09-19 10:56:51 -0300

    Removendo saída antiga do experimento 03

--- Script-Experimento-03.R
+++
@@ -1,8 +0,0 @@
-library(ExpDes.pt)
-attach(Dados_G3)
-
-Anova1 <- dbc(Consorcio, bloco, insetA)
-Anova2 <- dbc(Consorcio, bloco, MSN)
-Anova3 <- dbc(Consorcio,bloco, Nod)
-Anova4 <- dbc(Consorcio, bloco, Nod_F)
-Anova5 <- dbc(Consorcio, bloco, Prod)

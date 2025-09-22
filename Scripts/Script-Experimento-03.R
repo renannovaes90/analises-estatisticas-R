286889ec44af25d794a993f3e436c81d03062356
Renan Novaes <renan.cunha@protonmail.com>
2025-09-22 12:13:30 -0300

    Deletando estrutura do repositório antiga.

--- Scripts/Script-Experimento-03.R
+++
@@ -1,9 +0,0 @@
-library(ExpDes.pt)
-dados3 <- Dados_Experimento_03
-attach(dados3)
-
-Anova1 <- dbc(Consorcio, bloco, insetA)
-Anova2 <- dbc(Consorcio, bloco, MSN)
-Anova3 <- dbc(Consorcio,bloco, Nod)
-Anova4 <- dbc(Consorcio, bloco, Nod_F)
-Anova5 <- dbc(Consorcio, bloco, Prod)

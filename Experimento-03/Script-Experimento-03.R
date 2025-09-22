569bb73c801a56302d41b06b7c282ef879feebf4
Renan Novaes <renan.cunha@protonmail.com>
2025-09-22 13:07:03 -0300

    Adicionando informações ao código.

--- Experimento-03/Script-Experimento-03.R
+++ Experimento-03/Script-Experimento-03.R
@@ -1,9 +1,9 @@
-library(ExpDes.pt)
-dados3 <- Dados_Experimento_03
-attach(dados3)
+library(ExpDes.pt) #Carregando biblioteca ExpDes.pt
+dados3 <- Dados_Experimento_03 #Criando objeto para o dataset
+attach(dados3) #Anexando o dataset no R
 
-Anova1 <- dbc(Consorcio, bloco, insetA)
-Anova2 <- dbc(Consorcio, bloco, MSN)
-Anova3 <- dbc(Consorcio,bloco, Nod)
-Anova4 <- dbc(Consorcio, bloco, Nod_F)
-Anova5 <- dbc(Consorcio, bloco, Prod)
+Anova1 <- dbc(Consorcio, bloco, insetA) #Análise de variância para incidência de insetos adultos
+Anova2 <- dbc(Consorcio, bloco, MSN) #Análise de variância para matéria seca dos nódulos
+Anova3 <- dbc(Consorcio,bloco, Nod) #Análise de variância para nódulos
+Anova4 <- dbc(Consorcio, bloco, Nod_F) #Análise de variância para nódulos furados
+Anova5 <- dbc(Consorcio, bloco, Prod) #Análise de varância para produtividade 

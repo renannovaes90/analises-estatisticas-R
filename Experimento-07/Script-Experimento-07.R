bb4aa8d11c0e267fb67f4ec1d5a423f97ba2aa9b
Renan Novaes <renan.cunha@protonmail.com>
2025-09-22 17:41:53 -0300

    Acrescentando a transformação de Pcachos ao script.

--- Experimento-07/Script-Experimento-07.R
+++ Experimento-07/Script-Experimento-07.R
@@ -1,6 +1,8 @@
-library(ExpDes.pt) #carrega a biblioteca
+library(ExpDes.pt) #carrega a biblioteca para análise das ANOVAS
+library(nortest) #Carrega a biblioteca que faz as pressuposições (normalidade de resíduos e homogeneidade entre variâncias)
+library(MASS) #carrega a biblioteca que faz as transformações de dados
 dados7 <- Dados_Experimento_07 #criando um objeto para o database
-attach(dados7) #fixando o database
+attach(dados7) #fixando o database no R
 
 #Seguem ANOVAS com testes de médias utilizando o método Scott-Knott uma vez que o número de parcelas (n) é maior que 50
 ANOVA1 <- dic(Genotipos, Nfolhas, mcomp = "sk") #Análise de variância para número de folhas
@@ -10,21 +12,20 @@ ANOVA4 <- dic(Genotipos, Nfrutos, mcomp = "sk") #Análise de variância para nú
 ANOVA5 <- dic(Genotipos, Pcachos,mcomp = "sk") #Análise de variância para peso de cachos
 
 #Segue teste para normalidade de resíduos quando número de parcelas (n) > 50
-library(nortest)
 
 # Ajustando modelos lineares correspondentes
-modelo1 <- aov(Nfolhas ~ Genotipos, data = dados7) #Modelo linear para número defolhas
-modelo2 <- aov(SigatAmar ~ Genotipos, data = dados7) #Modelo linear para incidência de Sigatoka-amarela
-modelo3 <- aov(NPencas ~ Genotipos, data = dados7) #Modelo para número de pencas
-modelo4 <- aov(Nfrutos ~ Genotipos, data = dados7) #Modelo para número de frutos
-modelo5 <- aov(Pcachos ~ Genotipos, data = dados7) #Modelo para peso de cachos
+modelo_Nfolhas <- aov(Nfolhas ~ Genotipos, data = dados7) #Modelo linear para número defolhas
+modelo_SigatAmar <- aov(SigatAmar ~ Genotipos, data = dados7) #Modelo linear para incidência de Sigatoka-amarela
+modelo_NPencas <- aov(NPencas ~ Genotipos, data = dados7) #Modelo para número de pencas
+modelo_Nfrutos <- aov(Nfrutos ~ Genotipos, data = dados7) #Modelo para número de frutos
+modelo_Pcachos <- aov(Pcachos ~ Genotipos, data = dados7) #Modelo para peso de cachos
 
 # Aplicando teste de normalidade de resíduos Lilliefors (lillie.test) nos resíduos
-lillie.test(residuals(modelo1)) 
-lillie.test(residuals(modelo2))
-lillie.test(residuals(modelo3))
-lillie.test(residuals(modelo4))
-lillie.test(residuals(modelo5))
+lillie.test(residuals(modelo_Nfolhas)) 
+lillie.test(residuals(modelo_SigatAmar))
+lillie.test(residuals(modelo_NPencas))
+lillie.test(residuals(modelo_Nfrutos))
+lillie.test(residuals(modelo_Pcachos))
 
 # Teste de homogeneidade de variâncias (Bartlett)
 
@@ -34,13 +35,16 @@ bartlett.test(NPencas ~ Genotipos, data = dados7)     # modelo3
 bartlett.test(Nfrutos ~ Genotipos, data = dados7)     # modelo4
 bartlett.test(Pcachos ~ Genotipos, data = dados7)     # modelo5
 
-#A normalidade de resíduos para Nfolhas não foi atendida (P<5%)
-#As pressuposições, normalidade de resíduos e homogeneidade não foram atendidas para SigatAmar. Bartlett e Lilliefors P<5%
-#Faz-se necessário a transformação dos dados com box-cox.
+##A normalidade de resíduos para Nfolhas não foi atendida (P<5%)
+##As pressuposições, normalidade de resíduos e homogeneidade não foram atendidas para SigatAmar. Bartlett e Lilliefors P<5%
+##Faz-se necessário a transformação dos dados com box-cox.
 
-#Segue transformação para Nfolhas
-library(MASS)
-modelo1 <- aov(Nfolhas ~ Genotipos, data = dados7)
+#-----------------------------
+# Transformação para Nfolhas
+#-----------------------------
+
+#Ajustando o modelo linear para Nfolhas 
+modelo1 <- aov(dados7$Nfolhas ~ dados7$Genotipos, data = dados7)
 
 # Box-Cox para sugerir transformação
 boxcox(modelo1, lambda = seq(-2, 2, 0.1))
@@ -56,3 +60,53 @@ modelo1_bc <- aov(Nfolhas_bc ~ Genotipos, data = dados7)
 library(nortest)
 lillie.test(residuals(modelo1_bc))                     # normalidade (Lilliefors)
 bartlett.test(Nfolhas_bc ~ Genotipos, data = dados7)   # homogeneidade
+
+#-------------------------------------
+# Segue transformação para SigatAmar
+#-------------------------------------
+
+# Ajustando o modelo linear para SigatAmar
+
+modelo2 <- aov(SigatAmar ~ Genotipos, data = dados7)
+
+# Box-Cox para identificar o lambda
+boxcox(modelo2, lambda = seq(-2, 2, 0.1))
+
+# Aplicando transformação com lambda ≈ -0.9
+lambda_sigatamar <- -0.9
+dados7$SigatAmar_bc <- (dados7$SigatAmar^lambda_sigatamar - 1) / lambda_sigatamar
+
+# Novo modelo com a variável transformada
+modelo2_bc <- aov(SigatAmar_bc ~ Genotipos, data = dados7)
+
+# Testes de pressuposições
+library(nortest)
+lillie.test(residuals(modelo2_bc))                     # normalidade
+bartlett.test(SigatAmar_bc ~ Genotipos, data = dados7) # homogeneidade
+
+# ---------------------------
+# Transformação para Pcachos
+# --------------------------
+
+# Ajustando modelo linear para Pcachos
+modelo5 <- aov(Pcachos ~ Genotipos, data = dados7)
+
+# Análise Box-Cox para sugerir a transformação
+boxcox(modelo5, lambda = seq(-2, 2, 0.1))
+
+# Substituir pelo valor de lambda indicado pelo gráfico
+lambda_pcachos <- -0.9   
+
+# Criando variável transformada para Pcachos
+dados7$Pcachos_bc <- (dados7$Pcachos^lambda_pcachos - 1) / lambda_pcachos
+
+# Novo modelo com transformação
+modelo5_bc <- aov(Pcachos_bc ~ Genotipos, data = dados7)
+
+# Testes de pressuposições
+lillie.test(residuals(modelo5_bc))                     # normalidade (Lilliefors ou Shapiro-Wilk)
+bartlett.test(Pcachos_bc ~ Genotipos, data = dados7)   # homogeneidade
+
+# ANOVA e teste de Scott-Knott após transformação
+ANOVA5_bc <- dic(Genotipos, dados7$Pcachos_bc, mcomp = "sk")
+

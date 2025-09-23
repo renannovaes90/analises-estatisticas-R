6e06793d8e0db05e7d9456adc95ab25253c465d5
Renan Novaes <renan.cunha@protonmail.com>
2025-09-23 14:40:29 -0300

    Editando script do experimento 11.

--- Experimento-11/Script-experimento-11.R
+++ Experimento-11/Script-experimento-11.R
@@ -3,4 +3,4 @@ dados11 <- Dados_experimento_11 #Criando objeto para o dataset
 attach(dados11) #Anexando dataset ao R
 
 ANOVA1 <- dic(K2O, NF) #Análise de variância para numero médio de frutos por planta
-ANOVA2 <- dic(K2O, PF) #Análise de variância para número médio de peso de frutos por planta
+ANOVA2 <- dic(K2O, PF) #Análise de variância para peso médio de frutos por planta
